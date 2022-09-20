c119208e31d8056f9f385849fd5c907f90dfd313
Jacquin Grégoire <gregoire.jacquin@me.com>
2022-09-20 14:18:33 +0200

    Add binding repository and controller

--- src/main/java/com/boardgame/geek/borrow/BorrowController.java
+++ src/main/java/com/boardgame/geek/borrow/BorrowController.java
@@ -1,27 +1,65 @@
 package com.boardgame.geek.borrow;
 
+import com.boardgame.geek.game.*;
+import com.boardgame.geek.user.UserInfo;
+import com.boardgame.geek.user.UserInfoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class BorrowController {
-
+    @Autowired
+    UserInfoRepository userInfoRepository;
+    @Autowired
+    CategoryRepository categoryRepository;
+    @Autowired
+    BorrowRepository    borrowRepository;
+    @Autowired
+    GameRepository      gameRepository;
     @GetMapping(value = "/borrows")
     public ResponseEntity getMyBorrow() {
-        //TODO
-        Borrow borrow = new Borrow();
-        borrow.setAskDate(LocalDate.now());
-        return new ResponseEntity(borrow,HttpStatus.OK);
+        List<Borrow> borrows = borrowRepository.findByBorrowerId(GameController.getUserConnectedId());
+        return new ResponseEntity(borrows,HttpStatus.OK);
     }
     @PostMapping("/borrows/{gameId}")
     public ResponseEntity addBorrow(@PathVariable("gameId") String gameId){
-        return new ResponseEntity<>(HttpStatus.CREATED);
+        Integer userConnectedId = GameController.getUserConnectedId();
+        Optional<UserInfo> borrower = userInfoRepository.findById(userConnectedId);
+        Optional<Game> game = gameRepository.findById(Integer.valueOf(gameId));
+        if(borrower.isPresent() && game.isPresent() && game.get().getGameStatus().equals(GameStatus.FREE)){
+            Borrow borrow = new Borrow();
+            Game bookEntity = game.get();
+            borrow.setGame(bookEntity);
+            borrow.setBorrower(borrower.get());
+            borrow.setLender(bookEntity.getUser());
+            borrow.setAskDate(LocalDate.now());
+            borrowRepository.save(borrow);
+
+            bookEntity.setGameStatus(GameStatus.BORROWED);
+            gameRepository.save(bookEntity);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }
+        return new ResponseEntity(HttpStatus.BAD_REQUEST);
     }
     @DeleteMapping("/borrows/{borrowId}")
     public ResponseEntity deleteBorrow(@PathVariable("borrowId") String borrowId){
+        Optional<Borrow> borrow = borrowRepository.findById(Integer.valueOf(borrowId));
+        if(!borrow.isPresent())
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        Borrow borrowEntity = borrow.get();
+        borrowEntity.setCloseDate(LocalDate.now());
+        borrowRepository.save(borrowEntity);
+
+        Game game = borrowEntity.getGame();
+        game.setGameStatus(GameStatus.FREE);
+        gameRepository.save(game);
+
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 }
--- src/main/java/com/boardgame/geek/user/UserInfoController.java
+++ src/main/java/com/boardgame/geek/user/UserInfoController.java
@@ -1,21 +1,27 @@
 package com.boardgame.geek.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.jws.soap.SOAPBinding;
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
-public class UserController {
-
+public class UserInfoController {
+    @Autowired
+    UserInfoRepository  userInfoRepository;
     @PostMapping(value = "/users")
     public ResponseEntity addUser(@Valid @RequestBody UserInfo user){
-        //TODO persist user
-        UserInfo localUser = new UserInfo();
-        localUser.setEmail("greg@local.com");
+        List<UserInfo> users = userInfoRepository.findByEmail(user.getEmail());
+        if(!users.isEmpty())
+            return new ResponseEntity("User already existing",HttpStatus.BAD_REQUEST);
+        userInfoRepository.save(user);
+
         return new ResponseEntity(user, HttpStatus.CREATED);
     }
 }
